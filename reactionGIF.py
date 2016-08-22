524954dcc67d4e1eb5127d0bbab21200dee674ec
Yogesh Dhamija <ydhamija96@gmail.com>
2016-08-22 16:12:47 -0400

    Added a much needed search function

--- reactionGIF.py
+++ reactionGIF.py
@@ -1,25 +1,36 @@
 from imgurpython import ImgurClient
 import random
 import subprocess
+import sys
+from difflib import SequenceMatcher
 
 client_id = "e2d66c0fbd32315"
 client_secret = "0ff533a2e1d7957666ff8ff5dd21a849b3dde3dc"
 
 client = ImgurClient(client_id, client_secret)
 
-albums = []
-count = 0
-for album in client.get_account_albums("reactiongifsarchive"):
-    album_title = album.title if album.title else 'Untitled'
-    print('{1}: {0}'.format(album_title, count))
-    albums.append(album.id)
-    count = count+1
+if(len(sys.argv) <= 1):
+    albums = []
+    count = 0
+    for album in client.get_account_albums("reactiongifsarchive"):
+        album_title = album.title if album.title else 'Untitled'
+        print('{1}: {0}'.format(album_title, count))
+        albums.append(album.id)
+        count = count+1
+    print()
+    choice = int(input("Please pick your choice: "))
+    print()
+    chosAlb = albums[choice]
+else:
+    chosAlb = -1
+    for album in client.get_account_albums("reactiongifsarchive"):
+        if(sys.argv[1].lower() in album.title.lower()):
+            chosAlb = album.id
+    if chosAlb == -1:
+        print("404 Category Not Found")
+        exit()
 
-print()
-choice = int(input("Please pick your choice: "))
-print()
-
-theChosen = random.choice(client.get_album_images(albums[choice]))
+theChosen = random.choice(client.get_album_images(chosAlb))
 
 cmd = 'echo '+theChosen.link.strip()+'|clip'
 subprocess.check_call(cmd, shell=True)
